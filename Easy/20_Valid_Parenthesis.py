bf9b692f9a1dda630db970cd3a1fc047c609007c
Dayanand K <dayanandeee2004@gmail.com>
2025-06-12 08:19:57 +0530

    Add Solution for Valid Parenthesis - Easy

---
+++ Easy/20_Valid_Parenthesis.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/valid-parentheses/description/
+def isValid(s):
+  stack = []
+  mapping = {')': '(', '}': '{', ']': '['}
+  for char in s:
+    if char in mapping:
+      top = stack.pop() if stack else '#'
+      if mapping[char] != top:
+        return False
+    else:
+      stack.append(char)
+  return not stack
+
+result = isvalid("({[]})")
+print(result)
